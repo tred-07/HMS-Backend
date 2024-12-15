caf9f956c0cf402381263b3bb80427a180193e61
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-12-16 02:20:35 +0600

    done

--- doctor/views.py
+++ doctor/views.py
@@ -4,18 +4,27 @@ from .models import AvailableTime,Designation,Doctor,Review,Specialization
 from . import serializers
 from rest_framework.permissions import IsAuthenticated
 
+class AvailableTimeForSpecificDoctor(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        doctor_id=request.query_params.get("doctor_id")
+        if doctor_id:
+            return queryset.filter(doctor=doctor_id)
+        return queryset
+
+class AvailableTimeViewSets(viewsets.ModelViewSet):
+    queryset=AvailableTime.objects.all()
+    serializer_class=serializers.AvailableTimeSerializer
+    filter_backends=[AvailableTimeForSpecificDoctor]
+
+class DesignationViewSets(viewsets.ModelViewSet):
+    queryset=Designation.objects.all()
+    serializer_class=serializers.DesignationSerializer
+
 
 class DoctorPagination(pagination.PageNumberPagination):
     page_size=1 # items per page
     page_size_query_param=page_size
     max_page_size=100
-class AvailableTimeViewSets(viewsets.ModelViewSet):
-    queryset=AvailableTime.objects.all()
-    serializer_class=serializers.AvailableTimeSerializer
-
-class DesignationViewSets(viewsets.ModelViewSet):
-    queryset=Designation.objects.all()
-    serializer_class=serializers.DesignationSerializer
 
 class DoctorViewSets(viewsets.ModelViewSet):
     queryset=Doctor.objects.all()
